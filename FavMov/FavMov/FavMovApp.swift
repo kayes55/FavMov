8f0bb96002a2971bc4de90264aadcc8b43f88dcf
Kayes <kayes.shamil@gmail.com>
2024-02-16 15:31:10 +0600

    Project Initialized

---
+++ FavMov/FavMov/FavMovApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavMovApp.swift
+//  FavMov
+//
+//  Created by Kayes on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FavMovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
